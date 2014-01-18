a7fcb16774cb3708609fd7e4e6cf3b051bfe9dc1
Jose Antunes <theholy7@gmail.com>
2014-01-18 13:32:26 +0000

    Initial commit: druglord.py

---
+++ druglord.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#Define city classes
+class City (object):
+
+
+#Define user classes
+class User (object):
+
+
+#Main game function
+def main():
+
+
+
+if __name__ == '__main__':
+	main()
