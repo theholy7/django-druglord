12cf3f4b3cddbb7a702a4acefb199bbfadfd644e
Jose Antunes <theholy7@gmail.com>
2014-01-21 23:19:38 +0000

    add(druglord.py): Move City
    Add PLAYER_LIST and allows multiple players

--- druglord.py
+++ druglord.py
@@ -6,10 +6,12 @@
 
 DEFAULT_DAYS = 3
 CITY_LIST = []
+PLAYER_LIST = []
 
 class City (object):
 	def __init__ (self, name):
 		self.name = name
+		CITY_LIST.append(self)
 
 
 #Define user classes
@@ -17,9 +19,10 @@ class User (object):
 	def __init__(self, name, city):
 		self.name = name
 		self.city = city
+		PLAYER_LIST.append(self)
 
 	def get_city(self):
-		print self.city.name
+		return self.city.name
 
 	def move_city(self, new_city):
 		self.city = new_city
@@ -40,29 +43,43 @@ def game(day = 1, max_days = DEFAULT_DAYS):
 	lisbon = City("Lisbon")
 	porto = City("Porto")
 
-	CITY_LIST.append(lisbon)
-	CITY_LIST.append(porto)
-
 	jose = User("jose", lisbon)
 
 	while(day <= max_days):
-		print "DAY NUMBER %d" % day
-		game_actions()
-		action = raw_input(">")
 
-		if action == "1":
-			for city in CITY_LIST:
-				print city.name
+		for player in PLAYER_LIST:
 
-			print "\n Choose new city number:"
-			new_city = raw_input(">")
+			print "DAY NUMBER {} of {}".format(day, player.name)
 
-			try:
-				if int(new_city)
-			except Exception, e:
-				raise e
-			if new_city.lower() in [x.name.lower() for x in CITY_LIST]:
-				new
+			game_actions()
+			action = raw_input(">")
+			
+
+			if action == "1":
+				for (i, city) in enumerate(CITY_LIST):
+					print city.name
+				while True:
+					print "\n Choose new city number or Q to go back:"
+					new_city = raw_input(">")
+					
+					if new_city == "q":
+						break
+
+					try:
+						try:
+							new_city = CITY_LIST[int(new_city)]
+							print "{} was in {}".format(jose.name, jose.get_city())
+							jose.move_city(new_city)
+							print "{} is now in {}".format(jose.name, jose.get_city())
+							break
+						except IndexError:
+							print("{} is not a valid city.".format(int(new_city)))
+
+					except ValueError:
+						pass
+						# try:
+						# 	if new_city.lower() in [x.name.lower() for x in CITY_LIST]:
+				
 
 
 
