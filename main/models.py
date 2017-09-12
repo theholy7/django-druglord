7e65223bca0999776bfe799cba0253c4aa5174fe
Jose Antunes <antunes.jose.mf@gmail.com>
2017-09-12 14:43:06 +0100

    Right relationships

--- main/models.py
+++ main/models.py
@@ -8,15 +8,16 @@ class Character(models.Model):
     cash = models.DecimalField(max_digits=19, decimal_places=2)
     danger = models.IntegerField()
 
-    on_day = models.OneToOneField(to=Day)
+    on_day = models.OneToOneField(to='Day', on_delete=models.CASCADE)
 
 
 class Day(models.Model):
     day_number = models.IntegerField(name="Day")
-    for_sale = models.ForeignKey(to=Product, on_delete=models.CASCADE)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=20)
-    quantity = models.IntegerField()
-    danger = models.IntegerField()
+    quantity = models.IntegerField(default=0)
+    danger = models.IntegerField(default=0)
+
+    for_sale = models.ForeignKey(to='Product', on_delete=models.CASCADE)
