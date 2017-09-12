837b5fccd7033f60e621bddf362f52601fdd8b2b
Jose Antunes <antunes.jose.mf@gmail.com>
2017-09-12 14:14:14 +0100

    Initial models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=20)
+    cash = models.DecimalField(max_digits=19, decimal_places=2)
+    danger = models.IntegerField()
+
+
+class Day(models.Model):
+    day_number = models.IntegerField(name="Day")
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    quantity = models.IntegerField()
+    danger = models.IntegerField()
