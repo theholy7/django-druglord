6e9e7ed92d22a7a37f68ff4ce057a8ebcb139371
Jose Antunes <antunes.jose.mf@gmail.com>
2017-09-25 11:18:04 +0100

    cycle through days

--- main/views.py
+++ main/views.py
@@ -1,7 +1,12 @@
+import logging
+
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from . import game_helpers, models
+
+logger = logging.getLogger(__name__)
+
 
 # Create your views here.
 
@@ -15,13 +20,19 @@ def home(request):
 
 
 def day(request, char_id, day_number):
+    try:
+        day_number = int(day_number)
+    except ValueError as e:
+        logger.error("Error with day_number: {}".format(e))
+
     char = get_object_or_404(models.Character, pk=char_id)
 
     day = game_helpers.create_game_day(day_number)
 
     context = {
         "char": char,
-        "day": day
+        "day": day,
+        "next_day": day.day_number + 1
     }
 
     return render(request, 'main/day.html', context=context)
